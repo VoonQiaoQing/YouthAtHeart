4ec0edf0ea3aabc75b033089c5bc39264ee93d3d
JEFFERY WONG JIA EN <191898Q@mymail.nyp.edu.sg>
2022-01-27 10:48:34 +0800

    fixed FAQ

--- Pages/FAQSend.cshtml.cs
+++ Pages/FAQSend.cshtml.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using YouthAtHeart.Models;
 
 namespace YouthAtHeart.Pages
 {
     public class FAQSendModel : PageModel
     {
+        [BindProperty]
+        public Questions MyQuestion { get; set; }
+
         public void OnGet()
         {
         }
